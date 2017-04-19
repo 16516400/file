0130d760fab4bc5b5ad0d73fbf364d7f8cb0a36a
16516400 <anak_gaul_hehehe@yahoo.com>
2017-04-19 20:46:56 +0700

    Create number.js

---
+++ number.js
@@ -0,0 +1,14 @@
+# file
+var annualLeave = 30
+var BobRemainingDays = 12
+var AliceRemainingDays = 28
+var TomRemainingDays = 19
+var average = ((annualLeave-BobRemainingDays)+(annualLeave-AliceRemainingDays)+(annualLeave-TomRemainingDays))/3
+
+exports.average = average
+exports.annualLeave = annualLeave
+exports.BobRemainingDays = BobRemainingDays
+exports.AliceRemainingDays = AliceRemainingDays
+exports.TomRemainingDays = TomRemainingDays
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
